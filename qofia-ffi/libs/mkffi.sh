a79cfc33ea435bf6454956098f81401b5b4d5884
envoy <envoy@solaris.com>
2019-03-07 17:13:36 +0800

    update build script

--- qofia-ffi/libs/mkffi.sh
+++ qofia-ffi/libs/mkffi.sh
@@ -4,14 +4,23 @@ SYSROOT=/opt/android-ndk/sysroot
 # CPP="arm-linux-androideabi-gcc -E" CXXCPP="arm-linux-androideabi-gcc -E" CPPFLAGS="-I${SYSROOT}/usr/include" ./configure --prefix="${SYSROOT}/androidsys" --host=arm-linux-androideabi --target=arm-linux-androideabi CFLAGS="--sysroot $SYSROOT"
 
 export PATH=/opt/andndk16/bin:$PATH
-export CC=arm-linux-androideabi-gcc
-export CXX=arm-linux-androideabi-g++
-export CPP=arm-linux-androideabi-cpp
+if [[ -f /opt/andndk16/bin/arm-linux-androideabi-gcc ]]; then
+    export CC=arm-linux-androideabi-gcc
+    export CXX=arm-linux-androideabi-g++
+    export CPP=arm-linux-androideabi-cpp
 
-./configure --prefix=/androidsys --host arm-linux-androideabi
+    ./configure --prefix=/opt/androidsys --host arm-linux-androideabi
+else # android-x86
+    export CC=i686-linux-android-gcc
+    export CXX=i686-linux-android-g++
+    export CPP=i686-linux-android-cpp
+
+    ./configure --prefix=/opt/androidsys --host i686-linux-android
+fi
+
 
 make && make install
 
 
 # replace configure:21196 soname_spec=$lt_soname_spec to soname_spec=libffi.so
-#xxx broken arm elf, patchelf --set-soname libffi.so /androidsys/lib/libffi.so
+#xxx broken arm elf, patchelf --set-soname libffi.so /opt/androidsys/lib/libffi.so
--- qofia-ffi/libs/mkoal.sh
+++ qofia-ffi/libs/mkoal.sh
@@ -5,17 +5,23 @@ SYSROOT=/opt/android-ndk/sysroot
 # CPP="arm-linux-androideabi-gcc -E" CXXCPP="arm-linux-androideabi-gcc -E" CPPFLAGS="-I${SYSROOT}/usr/include" ./configure --prefix="${SYSROOT}/androidsys" --host=arm-linux-androideabi --target=arm-linux-androideabi CFLAGS="--sysroot $SYSROOT"
 
 export PATH=/opt/andndk16/bin:$PATH
-export CC=arm-linux-androideabi-gcc
-export CXX=arm-linux-androideabi-g++
-export CPP=arm-linux-androideabi-cpp
+if [[ -f /opt/andndk16/bin/arm-linux-androideabi-gcc ]]; then
+    export CC=arm-linux-androideabi-gcc
+    export CXX=arm-linux-androideabi-g++
+    export CPP=arm-linux-androideabi-cpp
+else # android-x86
+    export CC=i686-linux-android-gcc
+    export CXX=i686-linux-android-g++
+    export CPP=i686-linux-android-cpp
+fi
 export CFLAGS="-fPIE -DANDROID -Wno-multichar -D__ANDROID_API__=16"
 
-cmake -DCMAKE_VERBOSE_MAKEFILE=on -DEXAMPLES=off -DCMAKE_INSTALL_PREFIX=/androidsys/ ..
+cmake -DCMAKE_VERBOSE_MAKEFILE=on -DEXAMPLES=off -DCMAKE_INSTALL_PREFIX=/opt/androidsys/ .
 
 echo ""
 echo "configure done. edit link.txt, change soname without version suffix libopenal.so"
 sleep 3
-make
+# make && make install 
 
 
 # source: https://github.com/AerialX/openal-soft-android
